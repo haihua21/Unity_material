2a7540ba670437d3c8a13ce8b4c1484056888e7b
haihua.zhang <15127833@qq.com>
2023-08-31 14:13:29 +0800

    Add model AutoRotating Script

---
+++ Assets/Art_Zhh/common/scripts/EffectAutoRotating.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class EffectAutoRotating : MonoBehaviour
+{
+    public Vector3 XYZspeed;
+
+    void Update()
+    {
+        // transform.Rotate(Vector3.right, Time.deltaTime * XYZspeed.x, Space.Self);
+        // transform.Rotate(Vector3.up, Time.deltaTime * XYZspeed.y, Space.Self);
+        // transform.Rotate(Vector3.forward, Time.deltaTime * XYZspeed.z, Space.Self);
+        transform.Rotate(XYZspeed * Time.deltaTime, Space.Self);
+    }
+}
