70f04b0ff47597248912195498c918a7f0024501
hhua21 <15127833@qq.com>
2023-10-20 01:36:43 +0800

    Add and Adjust Custom Bloom

---
+++ Assets/Art_Zhh/common/shader/render_features/bloom/Cus_Bloom.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.Rendering;
+
+[System.Serializable, VolumeComponentMenu("Custom/Cus_Bloom")] //添加到Volume 目录中
+public class Cus_Bloom : VolumeComponent //, IPostProcessComponent
+{
+    public IntParameter BloomTimes = new ClampedIntParameter(1, 0, 5);
+    public FloatParameter BloomRange = new ClampedFloatParameter(1.0f, 0.0f, 5.0f);
+    public IntParameter donwSample = new ClampedIntParameter(2, 1, 16);
+
+    //public FloatParameter intensity = new ClampedFloatParameter(1, 0, 10);
+    public FloatParameter centerX = new ClampedFloatParameter(0.5f, 0, 1);
+    public FloatParameter centerY = new ClampedFloatParameter(0.5f, 0, 1);
+}
